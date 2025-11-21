3fac5bca5fb7e16eda2982eee5bb8e873392f52e
Satori <satriya.wicaksana@phincon.com>
2025-11-21 11:12:04 +0700

    add get request and error handling

--- Package.swift
+++ Package.swift
@@ -5,17 +5,25 @@ import PackageDescription
 
 let package = Package(
     name: "GeneralNetworkingHandler",
+    platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "GeneralNetworkingHandler",
             targets: ["GeneralNetworkingHandler"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.10.2")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "GeneralNetworkingHandler"),
+            name: "GeneralNetworkingHandler",
+            dependencies: [
+                .product(name: "Alamofire", package: "Alamofire")
+            ]
+        ),
         .testTarget(
             name: "GeneralNetworkingHandlerTests",
             dependencies: ["GeneralNetworkingHandler"]
--- Sources/GeneralNetworkingHandler/GeneralNetworkingHandler.swift
+++ Sources/GeneralNetworkingHandler/GeneralNetworkingHandler.swift
@@ -1,2 +1,49 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+import Alamofire
+
+public protocol Networking {
+    func get<T: Decodable & Sendable>(_ url: URL) async throws -> T
+}
+
+public final class NetworkingHelper: Networking {
+    
+    public init() {}
+
+    public func get<T: Decodable & Sendable>(_ url: URL) async throws -> T {
+
+        return try await withCheckedThrowingContinuation { continuation in
+
+            AF.request(url)
+                .validate()
+                .responseDecodable(of: T.self) { response in
+        
+                    switch response.result {
+                    case .success(let decoded):
+                        continuation.resume(returning: decoded)
+
+                    case .failure(_):
+                        
+                        let statusCode = response.response?.statusCode
+                        let message = try? JSONDecoder().decode(ErrorResponse.self, from: response.data ?? Data()).unifiedMessage
+                        
+                        let apiError: ApiError
+                        
+                        switch statusCode {
+                        case 400: return apiError = .badRequest(message: message)
+                        case 401: return apiError = .unauthorized
+                        case 403: return apiError = .forbidden
+                        case 404: return apiError = .notFound
+                        case 500: apiError = .serverError(message: message)
+                        default:
+                            return apiError = .unknown(statusCode: statusCode, message: message)
+                        }
+                        
+                        continuation.resume(throwing: apiError)
+                    }
+                }
+        }
+    }
+}
